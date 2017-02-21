08b8e1ede39389180122b1edd37a0877bf9c1670
Danilo de Sousa Abreu <danilo-abreu@Danilos-MacBook-Pro.local>
2017-02-21 12:15:29 -0300

    Add Swagger UI Doc UI

--- src/main/java/br/com/dsasoft/walmart/test/config/SpringConfig.java
+++ src/main/java/br/com/dsasoft/walmart/test/config/SpringConfig.java
@@ -6,13 +6,22 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @Configuration
-@EnableWebMvc
 @ComponentScan(basePackages = { "br.com.dsasoft.walmart.test.resources" })
+@EnableWebMvc
+@EnableSwagger2
 public class SpringConfig extends WebMvcConfigurerAdapter {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations(
+				"classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**").addResourceLocations(
+				"classpath:/META-INF/resources/webjars/");
+
 		registry.addResourceHandler("/resources/**").addResourceLocations(
 				"/resources/");
 	}
