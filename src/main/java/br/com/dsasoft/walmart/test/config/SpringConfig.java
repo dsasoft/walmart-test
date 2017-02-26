e2b79048e8dcfd500fcc92984d650a59d1c1e7a7
Danilo de Sousa Abreu <danilo-abreu@Danilos-MacBook-Pro.local>
2017-02-26 14:36:51 -0300

    Inject MongoDB values from Properties file

--- src/main/java/br/com/dsasoft/walmart/test/config/SpringConfig.java
+++ src/main/java/br/com/dsasoft/walmart/test/config/SpringConfig.java
@@ -1,18 +1,43 @@
 package br.com.dsasoft.walmart.test.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import com.mongodb.MongoClient;
+
 @Configuration
-@ComponentScan(basePackages = { "br.com.dsasoft.walmart.test.resources", "br.com.dsasoft.walmart.test.service" })
+@ComponentScan(basePackages = { "br.com.dsasoft.walmart.test.resources",
+		"br.com.dsasoft.walmart.test.service" })
+@PropertySource("classpath:config/mongodb.properties")
 @EnableWebMvc
 @EnableSwagger2
 public class SpringConfig extends WebMvcConfigurerAdapter {
+	
+	@Value("${mongodb.url}")
+	private String mongodbUrl;
+
+	@Value("${mongodb.port}")
+	public Integer mongoPort;
+
+	@Bean
+	public MongoClient mongoTemplate() throws Exception {
+		MongoClient mongo = new MongoClient(mongodbUrl, mongoPort);
+		return mongo;
+	}
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
--- src/main/java/br/com/dsasoft/walmart/test/service/MapService.java
+++ src/main/java/br/com/dsasoft/walmart/test/service/MapService.java
@@ -2,40 +2,46 @@ package br.com.dsasoft.walmart.test.service;
 
 import org.mongojack.JacksonDBCollection;
 import org.mongojack.WriteResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import br.com.dsasoft.walmart.test.model.Outline;
 
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
 
-import br.com.dsasoft.walmart.test.model.Outline;
-
 @Service
 public class MapService {
 
-	private DB db;
+	@Value("${mongodb.collectionName}")
+	public String COLLECTION_NAME;
+	
+	@Value("${mongodb.db}")
+	public String dbName;
+
+	@Autowired
 	private MongoClient mongoClient;
-	private static final String COLLECTION_NAME = "walmart";
-	
-	@SuppressWarnings("deprecation")
-	public MapService() {
-		this.mongoClient = new MongoClient("localhost", 27017);
-		this.db = mongoClient.getDB("db");
-	}
-	
+
+	private DB db;
+
 	public String saveMap(Outline map) {
-		JacksonDBCollection<Outline, String> coll = JacksonDBCollection.wrap(db.getCollection(COLLECTION_NAME),
-				Outline.class, String.class);
+
+		JacksonDBCollection<Outline, String> coll = JacksonDBCollection.wrap(
+				db.getCollection(COLLECTION_NAME), Outline.class, String.class);
 		WriteResult<Outline, String> result = coll.insert(map);
-		
+
 		return result.getSavedId();
 	}
-	
+
+	@SuppressWarnings("deprecation")
 	public Outline retrieveMap(String name) {
-		JacksonDBCollection<Outline, String> coll = JacksonDBCollection.wrap(db.getCollection(COLLECTION_NAME),
-				Outline.class, String.class);
+		db = mongoClient.getDB(dbName);
+		JacksonDBCollection<Outline, String> coll = JacksonDBCollection.wrap(
+				db.getCollection(COLLECTION_NAME), Outline.class, String.class);
 		Outline result = coll.findOneById(name);
-		
-		if(result!=null)
+
+		if (result != null)
 			return result;
 		else
 			throw new RuntimeException("Map not found");
